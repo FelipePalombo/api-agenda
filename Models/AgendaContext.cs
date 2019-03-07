e8d751456c76a3bc85bd865589d6bbd1a4b756e2
Felipe Ribeiro <feliperibeiro.rp@gmail.com>
2019-03-07 20:59:31 -0300

    WebApi iniciada e feito Scaffolding da Database

---
+++ Models/AgendaContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace api_agenda.Models
+{
+    public partial class AgendaContext : DbContext
+    {
+        public AgendaContext()
+        {
+        }
+
+        public AgendaContext(DbContextOptions<AgendaContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Contatos> Contatos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySQL("server=localhost;port=3306;user=root;password=123456;database=Agenda");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.2-servicing-10034");
+
+            modelBuilder.Entity<Contatos>(entity =>
+            {
+                entity.ToTable("contatos", "agenda");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasColumnType("int(11)")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Idade)
+                    .HasColumnName("idade")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.Nome)
+                    .HasColumnName("nome")
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Sexo)
+                    .HasColumnName("sexo")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Telefone)
+                    .HasColumnName("telefone")
+                    .HasColumnType("int(11)");
+            });
+        }
+    }
+}
---
+++ Models/Contatos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace api_agenda.Models
+{
+    public partial class Contatos
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sexo { get; set; }
+        public int? Idade { get; set; }
+        public int? Telefone { get; set; }
+    }
+}
